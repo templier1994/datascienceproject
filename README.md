d13c3ebc45aecbd32f67323496feb1ce5c4d0146
Jérôme Amos <amos@icare.ch>
2025-01-08 11:35:38 +0100

    feat(model_evaluation): implement model evaluation

--- README.md
+++ README.md
@@ -7,16 +7,16 @@ This project is an end to end data science project used to train MLOps.
 
 1. Data Ingestion
 2. Data Validation
-3. Data Transformation-- Feature Engineering,Data Preprocessing
+3. Data Transformation -- Feature Engineering,Data Preprocessing
 4. Model Trainer
-5. Model Evaluation- MLFLOW,Dagshub
+5. Model Evaluation -- MLFLOW,Dagshub
 
 ## Workflows
 
-1. Update config.yaml  ## config.yaml = folder with input/output for all task in the pipeline
-2. Update schema.yaml  ## schema of the data (columns with type and target)
+1. Update config.yaml-- config.yaml = folder with input/output for all task in the pipeline
+2. Update schema.yaml  -- schema of the data (columns with type and target)
 3. Update params.yaml
-4. Update the entity  ##
+4. Update the entity -- Dataclass that contain config.
 5. Update the configuration manager in src config
 6. Update the components
 7. Update the pipeline 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -49,6 +50,21 @@ try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
     obj = ModelTrainerTrainingPipeline()
     obj.initiate_model_training()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\n x===========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model evaluation stage"
+
+try:
+
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = ModelEvaluationTrainingPipeline()
+    obj.initiate_model_evaluation()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\n x===========x")
 
 except Exception as e:
